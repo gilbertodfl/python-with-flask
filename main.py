b2413d520cb80ba42b5e55796b128a9c142f27be
gilbertodfl <gilbertofl@tjdft.jus.br>
2025-12-09 18:03:01 -0300

    adicionando  request no main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 from forms import FormCriarConta, FormLogin
 ## https://flask-wtf.readthedocs.io/en/stable/
 
@@ -30,6 +30,10 @@ def usuarios():
 def login():
     form_login = FormLogin()
     form_criar_conta = FormCriarConta()
+    if form_login.validate_on_submit() and 'botao_submit_login' in request.form:
+        print(f'Login com o email: {form_login.email.data} e senha: {form_login.password.data} e lembrar de mim: {form_login.remember_me.data}')
+    if form_criar_conta.validate_on_submit() and 'botao_submit_criar_conta' in request.form:
+        print(f'Criar conta com o username: {form_criar_conta.username.data}, email: {form_criar_conta.email.data} e senha: {form_criar_conta.password.data}')
     return render_template('login.html', form_login=form_login, form_criar_conta=form_criar_conta)
     
 
