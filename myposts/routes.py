45040e80f244a98f92f77dbdc7fa8aace0a671c3
gilbertodfl <gilbertofl@tjdft.jus.br>
2026-01-30 20:14:24 -0300

    excluir post no BD

--- myposts/routes.py
+++ myposts/routes.py
@@ -155,3 +155,15 @@ def exibir_post(post_id):
     else:
         form = None
     return render_template('post.html', post=post, form=form)
+
+@app.route('/post/<post_id>/excluir', methods=['GET', 'POST'])
+@login_required
+def excluir_post(post_id):
+    post = Post.query.get(post_id)
+    if current_user == post.autor:
+        database.session.delete(post)
+        database.session.commit()
+        flash('Post Excluído com Sucesso', 'alert-danger')
+        return redirect(url_for('home'))
+    else:
+        abort(403)
